bd20656a4774d7f276fa59e873b45c457861803f
altermo <>
2023-12-23 01:11:09 +0100

    M lua/ultimate-autopair/test/init.lua

--- lua/ultimate-autopair/test/init.lua
+++ lua/ultimate-autopair/test/init.lua
@@ -1,6 +1,5 @@
 local utils=require'ultimate-autopair.test.utils'
 local  M={}
-M.jobs={}
 function M.check_not_allowed_string(path)
     if vim.fn.executable('grep')==0 then
         utils.warning('Some of the required executables are missing for dev testing')
@@ -11,20 +10,21 @@ function M.check_not_allowed_string(path)
     local search=table.concat(blacklist,'\\|')
     local job=vim.fn.jobstart({'grep','-r','--exclude-dir=test',search,path},{on_stdout=function (_,data,_)
         for _,v in ipairs(data) do
-            if v~='' then
-                utils.warning('Found something not allowed: '..v:sub(v:sub(2):find(' ') or 1))
-            end
+            if v=='' then return end
+            utils.warning('Found something not allowed: '..v:sub(v:sub(2):find(' ') or 1))
         end
     end})
     debug.setmetatable(job,{name='grep',expected=1})
-    table.insert(M.jobs,job)
-end
-function M.check_other(_)
-    if vim.fn.has('nvim-0.9.0')==0 then
-        utils.warning('You have an older version of neovim than recommended')
-    end
-    if not pcall(require,'nvim-treesitter') then
-        utils.warning('nvim-treesitter not installed: most of treesitter specific behavior will not work')
+    local jobs={job}
+    for k,exitcode in ipairs(vim.fn.jobwait(jobs,5000)) do
+        local mt=getmetatable(jobs[k])
+        if exitcode==-1 then
+            utils.warning(('timeout `%s`'):format(mt.name))
+        elseif exitcode~=(mt.expected or 0) then
+            utils.warning(('job `%s` exited with code %s'):format(mt.name,exitcode))
+        else
+            utils.info(('`%s` ran successfully'):format(mt.name))
+        end
     end
 end
 function M.start()
@@ -34,19 +34,11 @@ function M.start()
     if _G.UA_DEV then
         M.check_not_allowed_string(path)
     end
-    M.check_other()
-    local exitcodes=vim.fn.jobwait(M.jobs,5000)
-    for k,exitcode in ipairs(exitcodes) do
-        local mt=getmetatable(M.jobs[k])
-        local name=mt.name
-        local expected=mt.expected or 0
-        if exitcode==-1 then
-            utils.warning(('timeout `%s`'):format(name))
-        elseif exitcode~=expected then
-            utils.warning(('job `%s` exited with code %s'):format(name,exitcode))
-        else
-            utils.info(('`%s` ran successfully'):format(name))
-        end
+    if vim.fn.has('nvim-0.9.0')==0 then
+        utils.warning('You have an older version of neovim than recommended')
+    end
+    if not pcall(require,'nvim-treesitter') then
+        utils.warning('nvim-treesitter not installed: most of treesitter specific behavior will not work')
     end
     require'ultimate-autopair.test.run'.run(root)
 end
