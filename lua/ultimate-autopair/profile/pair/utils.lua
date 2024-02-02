e8de3c41215aa516a3301ac3a2116dfc88523a9e
altermo <107814000+altermo@users.noreply.github.com>
2024-02-02 18:42:28 +0100

    M lua/ultimate-autopair/profile/pair/utils.lua

--- lua/ultimate-autopair/profile/pair/utils.lua
+++ lua/ultimate-autopair/profile/pair/utils.lua
@@ -1,9 +1,18 @@
 local M={}
 ---@param o ua.info
-function M.backwards_get_start_pair(o)
-    local info=(o.m --[[@as ua.prof.def.bs]]).info
-    for _,v in ipairs(info.pairs) do
+---@param somepairs ua.prof.def.pair[]
+function M.backwards_get_start_pair(o,somepairs)
+    for _,v in ipairs(somepairs) do
         if v.info.start_pair==o.line:sub(o.col-#v.info.start_pair+1,o.col-1) then
+            return v
+        end
+    end
+end
+---@param o ua.info
+---@param somepairs ua.prof.def.pair[]
+function M.backwards_get_end_pair(o,somepairs)
+    for _,v in ipairs(somepairs) do
+        if v.info.end_pair==o.line:sub(o.col-#v.info.end_pair+1,o.col-1) then
             return v
         end
     end
