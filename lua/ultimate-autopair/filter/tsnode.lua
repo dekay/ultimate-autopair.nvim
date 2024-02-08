55433dbebd66cfdadf284c2d7698ea478f80f655
altermo <107814000+altermo@users.noreply.github.com>
2024-02-08 12:01:46 +0100

    M lua/ultimate-autopair/filter/tsnode.lua

--- lua/ultimate-autopair/filter/tsnode.lua
+++ lua/ultimate-autopair/filter/tsnode.lua
@@ -16,4 +16,19 @@ function M.call(o)
     end
     return true
 end
+---@param o ua.filter
+---@return table?
+function M.pre_call(o)
+    local range={o.rows-1,o.cols-1,o.rowe-1,o.cole-1}
+    local parser=o.source.get_parser()
+    if not parser then return end
+    local nodes=query.find_all_node_types(parser,{})
+    for _,node in ipairs(nodes) do
+        local trange={node:range()}
+        if (trange[1]<range[1] or (trange[1]==range[1] and trange[2]<range[2])) and
+            (trange[3]>range[3] or (trange[3]==range[3] and trange[4]>range[4])) then
+            return {node=node}
+        end
+    end
+end
 return M
