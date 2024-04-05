d75e9474daac38e5919435a9ce942053b2df84a5
altermo <107814000+altermo@users.noreply.github.com>
2024-04-05 07:27:52 +0200

    ;fastwarp add end of word warp

--- lua/ultimate-autopair/profile/pair/map/fastwarp.lua
+++ lua/ultimate-autopair/profile/pair/map/fastwarp.lua
@@ -6,6 +6,20 @@ local putils=require'ultimate-autopair.profile.pair.utils'
 ---@class ua.prof.pair.fastwarp.conf:ua.prof.pair.map.conf
 
 local M={}
+---@type (fun(o:ua.info,ind:number,p:string):ua.actions|nil)[]
+M.act={
+    function (o,ind,p)
+        if not o.line:sub(ind,ind):match('%w') then return end
+        while o.line:sub(ind,ind):match('%w') do
+            ind=ind+1
+        end
+        return {
+            {'delete',0,#p},
+            {'pos',ind-1},
+            p,{'left',#p},
+        }
+    end
+}
 ---@param o ua.info
 ---@return ua.actions|nil
 function M.run(o)
@@ -13,6 +27,12 @@ function M.run(o)
     local info=m.info
     local epairs=putils.forward_get_end_pairs(o,info.pairs)
     for _,epair in ipairs(epairs) do
+        for col=o.col+#epair.info.end_pair,#o.line do
+            for _,v in ipairs(M.act) do
+                local ret=v(setmetatable({m=epair,col=col},{__index=o}),col,epair.info.end_pair)
+                if ret then return ret end
+            end
+        end
         if o.col~=#o.line then
             return {
                 {'delete',0,#epair.info.end_pair},
